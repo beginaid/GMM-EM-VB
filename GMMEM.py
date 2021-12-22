75be0022bb3ae4a9190871f067e19e26d91068b0
beginaid <zuka.info.official@gmail.com>
2021-12-22 21:12:25 +0900

    [modify] Introduce matrix calculation

--- GMMEM.py
+++ GMMEM.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import fire
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 from numpy import linalg as la
+from collections import Counter
+from scipy.special import logsumexp
+from mpl_toolkits.mplot3d import Axes3D
 
 class GMMEM():
     def __init__(self, K):
@@ -30,13 +31,32 @@ class GMMEM():
         # The size of X is (N, D)
         self.N, self.D = X.shape
         # Mean value of the Gaussian distribution is generate from standard Gaussian distribution
-        self.Mu = np.random.randn(self.K, self.D)
+        self.mu = np.random.randn(self.K, self.D)
         # Set the init variance-covariance matrix as an identity matrix
-        self.Sigma = np.array([np.eye(self.D) for i in range(self.K)])
+        self.sigma = np.array([np.eye(self.D) for i in range(self.K)])
         # Init weight of the mixed Gaussian distribution is generated from Uniform distribution
-        self.Pi = np.array([1/self.K for i in range(self.K)])
+        self.pi = np.array([1 / self.K for i in range(self.K)])
         # Responsibility of the input data is generate from standard Gaussian distribution
+        # Responsibility will be updated at E-step, and this initialization practically has no meaning
         self.r = np.random.randn(self.N, self.K)
+
+    def log_likelihood(self, X):
+        """Calculate the log-likelihood of the D-dimensional mixed Gaussian distribution at N data.
+
+        Args:
+            X (numpy ndarray): Input data whose size is (N, D).
+
+        Returns:
+            log_likelihood (numpy ndarray): Values of the D-dimensional Gaussian distribution at N data whose size is (N, K).
+
+        """
+        log_pi = np.tile(np.log(self.pi + np.spacing(1))[None, :], (self.N, 1)) # (N, K)
+        log_sigma_det = np.tile(np.log(la.det(self.sigma) + np.spacing(1))[None,:], (self.N, 1)) # (N, K)
+        res_error = np.tile(X[:,None,None,:], (1, self.K, 1, 1)) - np.tile(self.mu[None,:,None,:], (self.N, 1, 1, 1)) # (N, K, 1, D)
+        sigma_inv = np.tile((la.pinv(self.sigma)[None,:,:,:]), (self.N, 1, 1, 1)) # (N, K, D, D)
+        log_exponent = np.einsum("nkod, nkde -> nkoe", np.einsum("nkod, nkde -> nkoe", res_error, sigma_inv), res_error.transpose(0,1,3,2))[:,:,0,0] # (N, K)
+        log_likelihood = log_pi + (-0.5 * self.D * np.log(2 * np.pi)) + (-0.5 * log_sigma_det) + (-0.5 * log_exponent) # (N, K)
+        return log_likelihood # (N, K)
 
     def e_step(self, X):
         """Execute the E-step of EM algorithm.
@@ -49,13 +69,17 @@ class GMMEM():
 
         Note:
             The following parameters are optimized in this method.
-                self.r (numpy ndarray): Responsibility of each data whose size is (N, D).
+                self.r (numpy ndarray): Responsibility of each data whose size is (N, K).
 
         """
-        # Calculate the responsibilities
-        mixed_gaussian_values = self.mix_gauss(X)
-        # Update the responsibilities
-        self.r = mixed_gaussian_values / np.sum(mixed_gaussian_values, 0)
+        # Calculate the responsibilities in log-domain
+        log_likelihood = self.log_likelihood(X)
+        log_r = log_likelihood - logsumexp(log_likelihood, 1, keepdims=True)
+        # Modify to exponential-domain
+        r = np.exp(log_r)
+        # Replace the element where nan appears
+        r[np.isnan(r)] = 1.0 / (self.K)
+        self.r = r
 
     def m_step(self, X):
         """Execute the M-step of EM algorithm.
@@ -75,79 +99,15 @@ class GMMEM():
         """
         # Calculate the parameters (Mu, Sigma, Pi) that maximize the Q function
         # Prepare N_k to calculate the parameters
-        N_k = np.sum(self.r, 1)[:,None]
+        N_k = np.sum(self.r, 0) # (K)
         # Calculate and update the optimized Mu
-        self.Mu = (self.r @ X) / N_k
-        # Calculate the optimized Sigma
-        sigma_list = np.zeros((self.N, self.K, self.D, self.D))
-        for k in range(self.K):
-            for n in range(self.N):
-                sigma_list[n][k] = self.r[k][n] * (X[n] - self.Mu[k])[:,None] @ ((X[n] - self.Mu[k]))[None,:]
-        # Update the optimized Sigma
-        self.Sigma = np.sum(sigma_list, 0) / N_k[:,None]
+        self.mu = (self.r.T @ X) / (N_k[:, None] + np.spacing(1)) # (K, D)
+        # Calculate and update the optimized Sigma
+        r_d = np.tile(self.r[:,:,None], (1, 1, self.D)).transpose(1, 2, 0) # (K, D, N)
+        res_error = np.tile(X[:,:,None], (1, 1, self.K)).transpose(2, 1, 0) - np.tile(self.mu[:,:,None], (1, 1, self.N)) # (K, D, N)
+        self.sigma = np.einsum("kdn, kne -> kde", np.einsum("kdn, kdn -> kdn", r_d, res_error), res_error.transpose(0, 2, 1)) / (N_k[:,None,None] + np.spacing(1)) # (K, D, D)
         # Calculate and update the optimized Pi
-        self.Pi = N_k / self.N
-
-    def calc(self, x, mu, sigma):
-        """Calculate the value of the D-dimensional Gaussian distribution at each data.
-
-        Args:
-            x (numpy ndarray): Input data whose size is D.
-            mu (numpy ndarray): Mean value of the D-dimensional Gaussian distribution whose size is D.
-            sigma (numpy ndarray): Variance-covariance matrix of the D-dimensional Gaussian distribution whose size is (D, D).
-
-        Returns:
-            gaussian_value (numpy ndarray): Value of the D-dimensional Gaussian distribution at each data whose size is D.
-
-        """
-        # Calculate exponent of D-dimensional Gaussian distribution
-        exp = -0.5 * (x - mu).T @ la.inv(sigma).T @ (x - mu)
-        # Calculate denomin of D-dimensional Gaussian distribution
-        denomin = np.sqrt(la.det(sigma)) * (np.sqrt(2*np.pi) ** self.D)
-        gaussian_value = np.exp(exp) / denomin
-        return gaussian_value
-    
-    def gauss(self, X, mu, sigma):
-        """Calculate the values of the D-dimensional Gaussian distribution at N data.
-
-        Args:
-            X (numpy ndarray): Input data whose size is (N, D).
-            mu (numpy ndarray): Mean value of the D-dimensional Gaussian distribution whose size is D.
-            sigma (numpy ndarray): Variance-covariance matrix of the D-dimensional Gaussian distribution whose size is (D, D).
-
-        Returns:
-            gaussian_values (numpy ndarray): Values of the D-dimensional Gaussian distribution at N data whose size is (N, D).
-
-        """
-        gaussian_values = np.array([self.calc(X[i], mu, sigma) for i in range(self.N)])
-        return gaussian_values
-    
-    def mix_gauss(self, X):
-        """Calculate the values of the D-dimensional mixed Gaussian distribution at N data.
-
-        Args:
-            X (numpy ndarray): Input data whose size is (N, D).
-
-        Returns:
-            mixed_gaussian_values (numpy ndarray): Values of the 1-dimensional Gaussian distribution at N data whose size is (N, D).
-
-        """
-        mixed_gaussian_values = np.array([self.Pi[i] * self.gauss(X, self.Mu[i], self.Sigma[i]) for i in range(self.K)])
-        return mixed_gaussian_values
-
-    def log_likelihood(self, X):
-        """Calculate the log-likelihood of the D-dimensional mixed Gaussian distribution at N data.
-
-        Args:
-            X (numpy ndarray): Input data whose size is (N, D).
-
-        Returns:
-            log_likelihood (numpy ndarray): Values of the 1-dimensional Gaussian distribution at N data whose size is (N, D).
-
-        """
-        mixed_gaussian_values = self.mix_gauss(X)
-        log_likelihood = np.sum([np.log(np.sum(mixed_gaussian_values, 0)[n]) for n in range(self.N)])
-        return log_likelihood
+        self.pi = N_k / self.N # (K)
 
     def classify(self, X):
         """Execute the classification.
@@ -159,7 +119,7 @@ class GMMEM():
             labels (numpy ndarray): Maximum values of the responsibility at each data whose size is N.
 
         """
-        labels = np.argmax(self.r, 0)
+        labels = np.argmax(self.r, 1) # (N)
         return labels
 
     def visualize(self, X):
@@ -180,12 +140,8 @@ class GMMEM():
         # Prepare the visualization
         fig = plt.figure(figsize=(4, 4), dpi=300)
         ax = Axes3D(fig)
-        # If the number of cluster is less than or equal to 8, use the custome color list.
-        if self.K <= 8:
-            color_list = ["#ec442c", "#067606", "#2474bc", "#940c7c", "#f27b6a", "#08a608", "#3a8ed9", "#c310a3"]         
-        # Otherwise, use the tab10 color map
-        else:
-            cm = plt.get_cmap("tab10")   
+        # Use the tab10 color map
+        cm = plt.get_cmap("tab10")   
         # Remove ticks
         ax.set_xticks([])
         ax.set_yticks([])
@@ -193,12 +149,10 @@ class GMMEM():
         # Change the perspective
         ax.view_init(elev=10, azim=70)
         # Visualize each clusters
-        for k in range(self.K):
-            cluster_indexes = np.where(labels==k)[0]
-            if self.K <= 8:
-                ax.plot(X[cluster_indexes, 0], X[cluster_indexes, 1], X[cluster_indexes, 2], "o", ms=0.5, color=color_list[k])
-            else:
-                ax.plot(X[cluster_indexes, 0], X[cluster_indexes, 1], X[cluster_indexes, 2], "o", ms=0.5, color=cm(k))
+        label_frequency_desc = [l[0] for l in Counter(labels).most_common()]
+        for k in range(len(label_frequency_desc)):
+            cluster_indexes = np.where(labels==label_frequency_desc[k])[0]
+            ax.plot(X[cluster_indexes, 0], X[cluster_indexes, 1], X[cluster_indexes, 2], "o", ms=0.5, color=cm(k))
         plt.show()
 
     def execute(self, X, iter_max, thr):
@@ -218,7 +172,7 @@ class GMMEM():
         # Prepare the list of the log_likelihood at each iteration
         log_likelihood_list = np.array([])
         # Calculate the initial log-likelihood
-        log_likelihood_list = np.append(log_likelihood_list, self.log_likelihood(X))
+        log_likelihood_list = np.append(log_likelihood_list, np.mean(self.log_likelihood(X)))
 
         # Start the iteration
         for i in range(iter_max):
@@ -227,12 +181,12 @@ class GMMEM():
             # Execute M-step
             self.m_step(X)
             # Add the current log-likelihood
-            log_likelihood_list = np.append(log_likelihood_list, self.log_likelihood(X))
+            log_likelihood_list = np.append(log_likelihood_list, np.mean(self.log_likelihood(X)))
             # Print the gap between the previous log-likelihood and current one
-            print("Previous log-likelihood gap: " + str(np.abs(log_likelihood_list[i] - log_likelihood_list[i+1])))
+            print("Log-likelihood gap: " + str(round(np.abs(log_likelihood_list[i] - log_likelihood_list[i+1]), 2)))
             # Visualization is performed when the convergence condition is met or when the upper limit is reached
             if (np.abs(log_likelihood_list[i] - log_likelihood_list[i+1]) < thr) or (i == iter_max - 1):
-                print(f"EM algorithm has stopped after {i + 1} iteraion")
+                print(f"EM algorithm has stopped after {i + 1} iteraion.")
                 self.visualize(X)
                 break
 
