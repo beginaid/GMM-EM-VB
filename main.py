da81563f3b65b9bdbd63e7b169eb2b261f314705
beginaid <zuka.info.official@gmail.com>
2021-12-31 21:39:33 +0900

    [modify] Change the data for clustering

--- main.py
+++ main.py
@@ -2,34 +2,41 @@ import numpy as np
 import sys
 import fire
 import GMMEM
+import GMMVB
 
-def createData(N):
+def createData():
     """Create 3-dimensional data that is divided into 4 clusters.
 
     Args:
-        N (int): The number of data.
+        None.
 
     Returns:
         X (numpy ndarray): Input data whose size is (N, 3).        
 
     """
+    # The number of data at each cluster
+    N1 = 4000
+    N2 = 3000
+    N3 = 2000
+    N4 = 1000
+
     # Mean values
-    Mu1 = [5, 5, 5]
-    Mu2 = [-5, -5, -5]
-    Mu3 = [-5, 5, 5]
-    Mu4 = [5, -5, -5]
-    
+    Mu1 = [5, -5, -5]
+    Mu2 = [-5, 5, 5]
+    Mu3 = [-5, -5, -5]
+    Mu4 = [5, 5, 5]    
+
     # Variance-covariance matrices
-    Sigma1 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    Sigma2 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    Sigma3 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    Sigma1 = [[1, 0, -0.25], [0, 1, 0], [-0.25, 0, 1]]
+    Sigma2 = [[1, 0, 0], [0, 1, -0.25], [0, -0.25, 1]]
+    Sigma3 = [[1, 0.25, 0], [0.25, 1, 0], [0, 0, 1]]
     Sigma4 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
     # Generate data from Gaussian distribution
-    X1 = np.random.multivariate_normal(Mu1, Sigma1, N)
-    X2 = np.random.multivariate_normal(Mu2, Sigma2, N)
-    X3 = np.random.multivariate_normal(Mu3, Sigma3, N)
-    X4 = np.random.multivariate_normal(Mu4, Sigma4, N)   
+    X1 = np.random.multivariate_normal(Mu1, Sigma1, N1)
+    X2 = np.random.multivariate_normal(Mu2, Sigma2, N2)
+    X3 = np.random.multivariate_normal(Mu3, Sigma3, N3)
+    X4 = np.random.multivariate_normal(Mu4, Sigma4, N4)   
 
     return np.concatenate([X1, X2, X3, X4])
 
@@ -51,12 +58,14 @@ def main(K, alg):
     """
     # Set the parameters of those algorithm
     iter_max = 100
-    thr = 0.01
+    thr = 0.001
     # Create data at each cluster whose size is 2500, resulting in 10000 data overall
-    X = createData(2500)
+    X = createData()
     # Instantiate the model
     if alg == "EM":
         model = GMMEM.main(K=K)
+    elif alg == "VB":
+        model = GMMVB.main(K=K)
     else:
         sys.stderr.write("Please specify the argument alg as EM or VB.")
     # Execute the algorithm
